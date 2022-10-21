965cf2d374fec36f8f4d0e6216db0b05f430e2a6
kadirkara22 <kadirkar2204@gmail.com>
2022-10-21 18:24:17 +0300

    my first project

---
+++ news_app/src/components/Banner/Banner.style.js
@@ -0,0 +1,9 @@
+import { Dimensions, StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    banner_container: {
+        height: Dimensions.get("window").height / 5,
+        width: Dimensions.get("window").width / 2,
+    }
+
+})
