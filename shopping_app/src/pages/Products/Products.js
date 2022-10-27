8849cc15f9d1826f528db958bb16dd92f748c7de
kadirkara22 <kadirkar2204@gmail.com>
2022-10-27 08:21:02 +0300

    lottie kullanılarak loading,error animasyonları eklendi

--- shopping_app/src/pages/Products/Products.js
+++ shopping_app/src/pages/Products/Products.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { FlatList, SafeAreaView, View, ActivityIndicator} from 'react-native';
+import { FlatList, SafeAreaView, View} from 'react-native';
 import Config from "react-native-config"
 import ProductCard from '../../components/ProductCard';
 import useFetch from "../../hooks/useFetch"
+import Loading from "../../components/Loading"
+import Error from "../../components/Error"
 const Products = () => {
    const {data,error,loading}=useFetch(Config.API_URL)
 
     const renderProduct = ({ item }) => <ProductCard product={item} />
 
 if(loading){
-    return <ActivityIndicator size="large" /> 
+    return <Loading /> 
 }
 if(error){
-    return <Text>{error}</Text> 
+    return <Error/> 
 }
     return (
         <SafeAreaView >
