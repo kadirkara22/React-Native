97a8b457ab61df14905cbee614677900b13a4677
kadirkara22 <kadirkar2204@gmail.com>
2022-11-06 21:49:22 +0300

    proje kurulumu ve gerekli paketler yüklendi

---
+++ codetalks/src/pages/ChatRooms/ChatRooms.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const ChatRooms = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default ChatRooms
